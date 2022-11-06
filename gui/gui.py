9cb8437dde1ba859dc964c1e091088233a4dcbc7
Czarek <czar9696@wp.pl>
2022-11-06 16:59:26 +0100

    Updating description

--- gui/gui.py
+++ gui/gui.py
@@ -123,4 +123,17 @@ class MainGUI:
             st.markdown(hide_table_row_index, unsafe_allow_html=True)
             st.table(df)
             st.write('While Davinci is generally the most capable, the other models can perform certain tasks extremely well with significant speed or cost advantages. For example, Curie can perform many of the same tasks as Davinci, but faster and for 1/10th the cost.')
-            st.write("We recommend using Davinci while experimenting since it will yield the best results. Once we’ve got things working, it's encourage trying the other models to see we you can get the same results with lower latency. We may also be able to improve the other models’ performance by fine-tuning them on a specific task.")
+            st.write("It's recommend using Davinci while experimenting since it will yield the best results.")
+
+            st.subheader('Davinci')
+            st.write('Davinci is the most capable model family and can perform any task the other models can perform and often with less instruction. For applications requiring a lot of understanding of the content, like summarization for a specific audience and creative content generation, Davinci is going to produce the best results. These increased capabilities require more compute resources, so Davinci costs more per API call and is not as fast as the other models.')
+            st.write('Another area where Davinci shines is in understanding the intent of text. Davinci is quite good at solving many kinds of logic problems and explaining the motives of characters. Davinci has been able to solve some of the most challenging AI problems involving cause and effect.')
+
+            st.subheader('Curie')
+            st.write('Curie is extremely powerful, yet very fast. While Davinci is stronger when it comes to analyzing complicated text, Curie is quite capable for many nuanced tasks like sentiment classification and summarization. Curie is also quite good at answering questions and performing Q&A and as a general service chatbot.')
+
+            st.subheader('Babbage')
+            st.write('Babbage can perform straightforward tasks like simple classification. It’s also quite capable when it comes to Semantic Search ranking how well documents match up with search queries.')
+
+            st.subheader('Ada')
+            st.write('Ada is usually the fastest model and can perform tasks like parsing text, address correction and certain kinds of classification tasks that don’t require too much nuance. Ada’s performance can often be improved by providing more context.')
